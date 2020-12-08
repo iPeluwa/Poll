7377e3030d61e44bbedf94d8f2e11775db21df2b
iPeluwa <ipeluwa@gmail.com>
2020-12-08 20:15:05 +0100

    second commit

--- database/connection.js
+++ database/connection.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 
 //mongoose.connect('mongodb://localhost:27017/PollsMERN');            //dev
 //mongoose.connect(process.env.DB_KEY);            //prod
-mongoose.connect("mongodb+srv://Himank:HimanksPolls@pollsmern-ggcgv.mongodb.net/test?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://bu:bu@ipeluwa.cv16q.mongodb.net/bu?retryWrites=true&w=majority");
 module.exports=mongoose;
